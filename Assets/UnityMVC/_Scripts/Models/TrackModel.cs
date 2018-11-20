8ef9a0151236086ab4c24b4614329c4e8dd877e9
Sourav Chatterjee <sromic@gmail.com>
2018-11-20 20:27:20 +0530

    [Update] Small fixes.

---
+++ Assets/UnityMVC/_Scripts/Models/TrackModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace UnityMVC._Scripts.Models
+{
+	public class TrackModel : MonoBehaviour
+	{
+		public List<int> Beats;
+	}
+}
