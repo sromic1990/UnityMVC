dc5fb37db5a05abf3a23c1deddbf23831fb882f8
Sourav Chatterjee <sromic@gmail.com>
2018-11-21 21:51:46 +0530

    [Update] Fixes.

---
+++ Assets/UnityMVC/_Scripts/View/TrackView.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityMVC._Scripts.Models;
+
+namespace UnityMVC._Scripts.View
+{
+	[RequireComponent(typeof(VerticalLayoutGroup), typeof(ContentSizeFitter), typeof(RectTransform))]
+	public class TrackView : MonoBehaviour
+	{
+		[SerializeField]private Track _track;
+	}
+}
